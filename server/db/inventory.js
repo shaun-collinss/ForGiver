1595cc4d28ad7af15199be3b12ef57f0ce5e94ed
shaun-collinss <draco_frostmore@hotmail.com>
2022-01-07 00:03:52 +1300

    tunin

--- server/db/inventory.js
+++ server/db/inventory.js
@@ -6,6 +6,8 @@ const connection = require('knex')(config)
 module.exports = {
   getInventory,
   getItem,
+  updateItem,
+  deleteItem
 }
 
 function getInventory (db = connection) {
@@ -16,4 +18,10 @@ function getItem (id, db = connection) {
   return db('inventory').select().where('id', id).first()
 }
 
+function updateItem(updateData, db = connection) {
+  return db('inventory').select().where({id}).update(updateData)
+}
 
+function deleteItem(id, db = connection) {
+  return db('inventory').select().where('id', id).delete()
+}
