6668275f10d9431eef5b853bb192cccf234d548b
shaun-collinss <draco_frostmore@hotmail.com>
2022-02-09 14:55:46 +1300

    latest

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -8,10 +8,12 @@ import Nav from './Nav'
 
 function Home () {
 const user = useSelector(state=>state.user)
+const inventory = useSelector(state => state.inventory)
 
   return (
     <>
       {user == 0? <h1>Welcome {user?.name}</h1>:<h1>Hi There login to start!</h1>}
+
     </>
   )
 }
